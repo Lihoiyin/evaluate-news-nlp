9e7df7f510981a6afc897df0113b45ed496e3fcb
Lihoiyin <88550451+Lihoiyin@users.noreply.github.com>
2021-08-22 22:13:13 +0800

    Add files via upload

---
+++ src/client/js/checkForUrl.js
@@ -0,0 +1,8 @@
+//https://thispointer.com/javascript-check-if-string-is-url/
+function checkForUrl(link) {
+    const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+    return matchpattern.test(link);
+  }
+
+
+export { checkForUrl }
