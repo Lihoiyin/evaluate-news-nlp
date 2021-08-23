de19bb710b3d0ecf4b8f27278db7111b873e0c4b
Lihoiyin <88550451+Lihoiyin@users.noreply.github.com>
2021-08-23 20:51:07 +0800

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new CssMinimizerWebpackPlugin({})],
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            // Reference (https://webpack.js.org/loaders/file-loader/)
+              //i dont know why file-loader didn't i can see the image when i use the image in my images file, so that i use link
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        new WorkboxPlugin.GenerateSW()
+    ]
+}
